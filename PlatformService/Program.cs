7ca2457de75afd1e0efb195cb2325e8bb5a01450
adi0546 <adityanarang010@gmail.com>
2024-03-18 22:43:15 -0400

    Add K8S resources for Ingress, PersistentVolumeClaim, and MSSQL deployment and services

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -8,8 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<AppDbContext>(opt => 
+if (builder.Environment.IsProduction())
+{
+    Console.WriteLine("--> Using SQL Server");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine("--> Using InMemDB");
+    builder.Services.AddDbContext<AppDbContext>(opt => 
     opt.UseInMemoryDatabase("InMem"));
+}
+
+
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddControllers();
